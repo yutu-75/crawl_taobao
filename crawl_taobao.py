f70ab2febd70d2407f2939ac74a89e3c7cca0ace
yanilo <xiao3952@foxmail.com>
2023-11-09 20:25:13 +0800

    update code1

--- crawl_taobao.py
+++ crawl_taobao.py
@@ -76,7 +76,7 @@ class CrawlTaoBao:
         self.cookies["_m_h5_tk"] = response.cookies.get("_m_h5_tk")
         self.cookies["_m_h5_tk_enc"] = response.cookies.get("_m_h5_tk_enc")
 
-    def search_commodity(self, name="女装", page_count=0, page_size=60):
+    def search_commodity(self, name="女装", page_count=1, page_size=60):
         """
         Args:
             name:
@@ -118,7 +118,25 @@ class CrawlTaoBao:
             cookies=self.cookies,
             headers=self.headers,
         )
-        return response.text
+
+        data = self.parse_json_from_string(response.text)['data']["recommend"]["resultList"]
+        commodity_information_list = []
+        for item in data:
+            dict1 = {
+                "item_name": item['itemName'],                                      # 衣服名字
+                "pic": item['pic'],                                                 # 图片
+                "price": item['price'],                                             # 价格
+                "monthSellCountFuzzyString": item['monthSellCountFuzzyString'],     # 月售
+                "provcity": item['provcity'],                                       # 产地
+                "item_id": item['itemId'],                                          # itemId
+            }
+            dict2 = self.get_detail(item['itemId'])
+            dict1.update(dict2)
+
+            commodity_information_list.append(dict1)
+            print(dict1)
+        print(commodity_information_list, len(commodity_information_list))
+        return commodity_information_list
 
     def get_detail(self, item_id="611658999337"):
         """
@@ -130,15 +148,36 @@ class CrawlTaoBao:
             f'https://item.taobao.com/item.htm?id={item_id}&ali_refid=a3_430582_1006:1110306571:N:emtiAWsF8%2Bzhhxaiwzc0Aw%3D%3D:2217fe1dd3fb55ad9851f45c3c3444f1&ali_trackid=1_2217fe1dd3fb55ad9851f45c3c3444f1&spm=a21n57.1.0.0',
             cookies=self.cookies, headers=self.headers)
         html = response.text
-        content = BeautifulSoup(html, "html.parser")
-        content = content.findAll("ul", attrs={"class": "attributes-list"})
-        for item in content:
-            age = item.findAll("li")[0].string  # 适合年龄
-            material = item.findAll("li")[2].string  # 适合材质
-            style = item.findAll("li")[4].string  # 风格
-            season = item.findAll("li")[-4].string  # 季节
-            # print(item)
-            print(age + "," + material + "," + style + "," + season)
+        # print(html, f'https://item.taobao.com/item.htm?id={item_id}&ali_refid=a3_430582_1006:1110306571:N:emtiAWsF8%2Bzhhxaiwzc0Aw%3D%3D:2217fe1dd3fb55ad9851f45c3c3444f1&ali_trackid=1_2217fe1dd3fb55ad9851f45c3c3444f1&spm=a21n57.1.0.0')
+        soup = BeautifulSoup(html, "html.parser")
+        attributes_html = soup.findAll("ul", attrs={"class": "attributes-list"})
+
+        details = {}
+        pattern_style = r'风格:\s(.*?)\n'
+        pattern_applicable_age = r'年龄:\s(.*?)\n'
+        pattern_fabric = r'面料.*?:\s(.*?)\n'
+        pattern_season = r'季节:\s(.*?)\n'
+
+        if attributes_html:
+            attributes_html = attributes_html[0].text
+
+            details = {
+                "style": re.findall(pattern_style, attributes_html)[0]
+                if re.findall(pattern_style, attributes_html) else None,
+
+                "applicable_age": re.findall(pattern_applicable_age, attributes_html)[0]
+                if re.findall(pattern_applicable_age, attributes_html) else None,
+
+                "fabric": re.findall(pattern_fabric, attributes_html)[0]
+                if re.findall(pattern_fabric, attributes_html) else None,
+
+                "season": re.findall(pattern_season, attributes_html)[0]
+                if re.findall(pattern_season, attributes_html) else None,
+            }
+
+            # print(details)
+
+        return details
 
     @staticmethod
     def parse_json_from_string(json_string):
@@ -182,17 +221,11 @@ if __name__ == '__main__':
     print(f"cookie>>>>>>:\n{crawl_taobao.cookies}\n")
     # crawl_taobao.get_detail("640513959729")
     # crawl_taobao.search_commodity("衣服", 1, 60)
-    # result = crawl_taobao.search_commodity("衣服")
-
-
-    # 搜索商品列表
-    result = crawl_taobao.search_commodity("露肩上衣一字肩直播衣服修身气质女主播设计感性感紧身斜肩t恤潮")
-
-    # 字符串json化
-    dict_data = crawl_taobao.parse_json_from_string(result)
-    print(dict_data)
-    for i in dict_data["data"]["recommend"]["resultList"]:
-        print("item_id", i["itemId"], f"https://item.taobao.com/item.htm?id={i['itemId']}")
-        # 获取商品详细信息
-        crawl_taobao.get_detail(i["itemId"])
-        break
+    crawl_taobao.search_commodity("女装")
+    # print(crawl_taobao.get_detail("702507248639"))
+    # print(content)
+    # detail=[]
+    # for itemid in itemId:
+    #     print(itemid)
+    #     detail.append(crawl_taobao.get_detail(itemid))
+    # print(detail)
