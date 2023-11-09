cda8903763aa6ae65929b3856635cc8002fcab15
yanilo <xiao3952@foxmail.com>
2023-11-09 11:02:48 +0800

    update code

--- crawl_taobao.py
+++ crawl_taobao.py
@@ -4,6 +4,7 @@ import time
 import hashlib
 import requests
 from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
 
 
 class CrawlTaoBao:
@@ -24,7 +25,10 @@ class CrawlTaoBao:
             'user-agent': UserAgent().random        # 随机生成一个User-Agent
         }
 
+        # Todo ["cna","sgcookie"] 生成方法没写, 待写...
         self.cookies = {
+            'cna': 'fHKVHZ23KEMCARsLEYfFlegU',
+            'sgcookie': 'E100fRiNqBPwDU36l9G1AiXsLbBcnOLqqJ1Xu%2BgWIgP2r%2B1s6bw8jOLpFJEmWTcWhCFSLVHJeetttNAZdmAQRDy4AEOzBLFohJpDVTHik4ifkJICcS7JFQ5AqZUQp1gyC%2Fmc',
             # 'isg': 'BB0dLh-OJPY-dsDauvWnwbDTLPkXOlGM_8mxTN_iXnSjlj3Ip4sZXOGAwIqQDmlE',
         }
         self.update_cookies()
@@ -116,31 +120,79 @@ class CrawlTaoBao:
         )
         return response.text
 
+    def get_detail(self, item_id="611658999337"):
+        """
+        获取商品详细信息:
+        Returns:
+
+        """
+        response = requests.get(
+            f'https://item.taobao.com/item.htm?id={item_id}&ali_refid=a3_430582_1006:1110306571:N:emtiAWsF8%2Bzhhxaiwzc0Aw%3D%3D:2217fe1dd3fb55ad9851f45c3c3444f1&ali_trackid=1_2217fe1dd3fb55ad9851f45c3c3444f1&spm=a21n57.1.0.0',
+            cookies=self.cookies, headers=self.headers)
+        html = response.text
+        content = BeautifulSoup(html, "html.parser")
+        content = content.findAll("ul", attrs={"class": "attributes-list"})
+        for item in content:
+            age = item.findAll("li")[0].string  # 适合年龄
+            material = item.findAll("li")[2].string  # 适合材质
+            style = item.findAll("li")[4].string  # 风格
+            season = item.findAll("li")[-4].string  # 季节
+            # print(item)
+            print(age + "," + material + "," + style + "," + season)
+
+    @staticmethod
+    def parse_json_from_string(json_string):
+        """
+        将字符串转换为 JSON
+        Args:
+            json_string:
+
+        Returns:
+
+        """
+        try:
+            match = re.search(r'mtopjsonp\d+\((.*?)\)', json_string)
+
+            if match:
+                json_str = match.group(1)
+                # 将 JSON 字符串解析为字典
+                parsed_json = json.loads(json_str)
+
+                return parsed_json
+        except json.JSONDecodeError as e:
+            print(f"JSON 解析错误：{e}")
+            return None
+
     @staticmethod
     def data_to_db(data):
+        """
 
-        match = re.search(r'mtopjsonp\d+\((.*?)\)', data)
+        Args:
+            data:
 
-        if match:
-            json_str = match.group(1)
-            # 将 JSON 字符串解析为字典
-            data = json.loads(json_str)
-            # 打印字典
-            print(data, type(data))
+        Returns:
 
-            # Todo 写解析出来的数据存到数据库里
-            ...
-        else:
-            print("JSON 数据未找到")
+        """
+        # Todo 写解析出来的数据存到数据库里
+        ...
 
 
 if __name__ == '__main__':
     crawl_taobao = CrawlTaoBao()
-
-    print(crawl_taobao.cookies)
-
-    result = crawl_taobao.search_commodity("衣服")
-    print(result)
-    crawl_taobao.data_to_db(result)
-    time.sleep(3)
+    print(f"cookie>>>>>>:\n{crawl_taobao.cookies}\n")
+    # crawl_taobao.get_detail("640513959729")
     # crawl_taobao.search_commodity("衣服", 1, 60)
+    # result = crawl_taobao.search_commodity("衣服")
+
+
+    # 搜索商品列表
+    result = crawl_taobao.search_commodity("露肩上衣一字肩直播衣服修身气质女主播设计感性感紧身斜肩t恤潮")
+
+    # 字符串json化
+    dict_data = crawl_taobao.parse_json_from_string(result)
+    print(dict_data)
+    for i in dict_data["data"]["recommend"]["resultList"]:
+        print("item_id", i["itemId"], f"https://item.taobao.com/item.htm?id={i['itemId']}")
+        # 获取商品详细信息
+        crawl_taobao.get_detail(i["itemId"])
+        break
